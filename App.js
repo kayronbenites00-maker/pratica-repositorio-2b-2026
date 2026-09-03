7f6062dfca7b11c421b997c90e3b80e8ff8a6ce9
perosso <muriloeduborges@gmail.com>
2026-09-03 11:09:52 -0300

    [feat] gluestack

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/global.css';
 
