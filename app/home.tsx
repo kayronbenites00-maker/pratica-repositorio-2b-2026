911d384c8eafb6e86d1485f38ba9215b9c1274f2
perosso <muriloeduborges@gmail.com>
2026-09-16 11:10:23 -0300

    commit

--- app/home.tsx
+++ app/home.tsx
@@ -1,11 +1,16 @@
+import { Box } from '@/components/ui/box';
 import { Text } from '@/components/ui/text';
+import { VStack } from '@/components/ui/vstack';
 import { Link } from 'expo-router';
 
 export default function home(){
   return (
-    <>
-    <Text>Pagina principal</Text>
-    <Link href='/Details'>Detalhes</Link>
-    </>
+    <Box className='flex-1 bg-white p-6'>
+      <VStack space="lg">
+
+        
+      </VStack>
+
+    </Box>
   )
 }
