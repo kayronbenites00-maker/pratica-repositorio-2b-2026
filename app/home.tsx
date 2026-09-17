766e8b9d9d4c51ae3255b096cf5e566e32bb551e
perosso <muriloeduborges@gmail.com>
2026-09-17 11:11:08 -0300

    commit

--- app/home.tsx
+++ app/home.tsx
@@ -4,9 +4,12 @@ import { Divider } from '@/components/ui/divider';
 import { Heading } from '@/components/ui/heading';
 import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
-import { Link } from 'expo-router';
+import { Link, useRouter } from 'expo-router';
 
 export default function home(){
+  const router = useRouter();
+
+
   return (
     <Box className='flex-1 bg-white p-6'>
       <VStack space="lg">
@@ -21,9 +24,29 @@ export default function home(){
       <ButtonText>Ir com o link (declarativo)</ButtonText>
     </Button>
     </Link>
+        <Divider />
         
-      </VStack>
 
+        <Button 
+        size="lg" 
+        onPress={()=> router.push("/Details")}
+          >
+      <ButtonText>Ir com router.push</ButtonText>
+        </Button>
+        <Divider />
+        <Button
+        size='lg'
+        variant='outline'
+        onPress={()=>
+          router.push({
+            pathname:"/detains",
+            params:{name: 'KayronBB'}
+          })
+        }
+        >
+          <ButtonText>Ir enviado um parametro</ButtonText>
+        </Button>
+      </VStack>
     </Box>
   )
 }
