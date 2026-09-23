765a1ca1717962920714830d43c2d270e61af7c5
perosso <muriloeduborges@gmail.com>
2026-09-23 10:00:09 -0300

    commit

--- app/Details.tsx
+++ app/Details.tsx
@@ -1,10 +1,30 @@
+import { Box } from '@/components/ui/box';
+import { boxStyle } from '@/components/ui/box/styles';
+import { Button, ButtonText } from '@/components/ui/button';
+import { Divider } from '@/components/ui/divider';
 import { Text } from '@/components/ui/text';
-import { Link } from 'expo-router';
+import { VStack } from '@/components/ui/vstack';
+import { Link, useLocalSearchParams, useRouter } from 'expo-router';
+
 export default function Details(){
+  const router = useRouter()
+  const {name} = useLocalSearchParams();
   return (
-    <>
-    <Text>detalhes</Text>
-    <Link href='/home'>Pagina Principal</Link>
-    </>
+    
+    <Box className='flex-1 bg-white p-6'>
+      <VStack space='lg'>
+    <Text size='2xl'>
+      essa é a rota "/Details" (arquivo app/Details.jsx)
+    </Text>
+    <Divider/>
+    <Text size='md'>
+      parametro recebido de outra tela:<Text className='font-extrabold'>{name? name : "sem parametro"}</Text>
+    </Text>
+    <Button size='lg' action="primary" onPress={()=> router.back()}>
+      <ButtonText >Voltar</ButtonText>
+    </Button>
+
+      </VStack>
+    </Box>
   )
 }
--- app/home.tsx
+++ app/home.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@/components/ui/box';
 import { Button, ButtonText } from '@/components/ui/button';
+import { Center } from '@/components/ui/center';
 import { Divider } from '@/components/ui/divider';
 import { Heading } from '@/components/ui/heading';
 import { Text } from '@/components/ui/text';
@@ -33,14 +34,16 @@ export default function home(){
           >
       <ButtonText>Ir com router.push</ButtonText>
         </Button>
-        <Divider />
+       <Center>
+        <Divider className="my-0.5 w-20 bg-emerald-500" />
+       </Center>
         <Button
         size='lg'
         variant='outline'
         onPress={()=>
           router.push({
-            pathname:"/detains",
-            params:{name: 'KayronBB'}
+            pathname:"/Details",
+            params:{name: '2° INfoxx'}
           })
         }
         >
